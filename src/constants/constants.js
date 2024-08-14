df33ed4105524345777ea90c6f1b405fcd98041a
BEJOY K K <kkbejoy@ymail.com>
2024-08-14 23:43:49 +0530

    api changed

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,8 @@
 const env = import.meta.env;
 export const BASE_URL = env.VITE_SERVER_URL;
 
-export const CLIENT_URL = "http://mentornudge.online";
+export const CLIENT_URL = "https://mentor-server-2bcz.onrender.com";
+  // "http://mentornudge.online";
 
 export const userTypes = {
   MENTOR: "mentor",
