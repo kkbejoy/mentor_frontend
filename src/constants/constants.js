dbc9ed9acef32e7c7e9b159aad72ae446c3ec160
BEJOY K K <kkbejoy@ymail.com>
2024-08-15 12:49:34 +0530

    server changes.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,8 @@
 const env = import.meta.env;
-export const BASE_URL = env.VITE_SERVER_URL;
+export const BASE_URL = "https://mentor-server-2bcz.onrender.com";
+  // env.VITE_SERVER_URL;
 
-export const CLIENT_URL = "https://mentor-server-2bcz.onrender.com";
+export const CLIENT_URL =  "http://mentornudge.online";
   // "http://mentornudge.online";
 
 export const userTypes = {
